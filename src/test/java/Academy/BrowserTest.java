a2a42377275de4e87b262d36adc2d7c34a0c3289
nischaytv <rocknischay@gmail.com>
2023-08-05 11:22:48 +0530

    updated assertions

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -22,7 +22,7 @@ public class BrowserTest {
 		driver.get("https://nisu.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase(" Nischay "));
+		Assert.assertTrue(text.equalsIgnoreCase("Nischay"));
 		driver.close();
 	
 		
